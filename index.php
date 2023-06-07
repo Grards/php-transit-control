a87192459abe4fa7c9c72023109583ec0768785e
Steve Grard <grard.steve@gmail.com>
2023-06-07 15:56:38 +0200

    Bases of project OK. SOME CSS active. TODO : formular + PHP

--- index.php
+++ index.php
@@ -4,9 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Forms</title>
+
+    <link rel="stylesheet" href="./assets/css/style.min.css">
+    <title>Transit Control</title>
 </head>
 <body>
-    
+    <header class="home-header">
+        <h1 class="header__title">Transit control</h1>
+        <section class="header__banner"></section>
+    </header>
+
+    <main class="home-main">
+        <section class="main__form">   
+            <form method="POST" action="" class="form__identity-control">
+                <fieldset>
+                    <legend>Identity informations</legend>
+
+                    <label for="firstname">First Name : </label>
+                    <input type="text" id="firstname">
+
+                    <label for="lastname">Last Name : </label>
+                    <input type="text" id="lastname">
+                </fieldset>
+                
+            </form>
+        </section>
+    </main>
 </body>
 </html>
